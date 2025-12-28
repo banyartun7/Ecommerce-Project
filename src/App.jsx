3d2bd78c4fda509312d85ea03bf54be4588e64fd
banyartun7 <bnyar012@gmail.com>
2025-12-28 22:46:58 +0630

    lift up cart it share between home page and checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import HomePage from './Pages/HomePage'
 import './App.css'
 import CheckoutPage from './Pages/checkout/CheckoutPage'
@@ -7,11 +9,20 @@ import TrackingPage from './Pages/TrackingPage'
 import PageNotFound from './Pages/PageNotFound'
 
 function App() {
+  const [cart, setCart] = useState([])
+
+  useEffect(() => {
+    axios.get("/api/cart-items")
+      .then((response) => {
+        setCart(response.data)
+    })
+  }, [])
+  
 
   return (
     <Routes>
-      <Route index element={<HomePage/>} />  
-      <Route path='/checkout' element={<CheckoutPage />} />
+      <Route index element={<HomePage cart = { cart } />} />  
+      <Route path='/checkout' element={<CheckoutPage cart = { cart } />} />
       <Route path='/order' element={<OrderPage />}/>
       <Route path='/tracking' element={<TrackingPage />} />
       <Route path='*' element={<PageNotFound />}></Route>
